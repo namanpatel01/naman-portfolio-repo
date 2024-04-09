9f463afc213a314834955587e5d4422e0ed1fd36
NamanPatel <namanpatel204@gmail.com>
2024-04-09 18:37:19 +0530

    Created working structure for App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"home",
+  component:HomeComponent
+},{
+  path:"home",
+  component:HomeComponent
+},
+{
+  path:"about",
+  component:AboutComponent
+},
+{
+  path:"projects",
+  component:ProjectsComponent
+},
+{
+  path:"contact",
+  component:ContactUsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
